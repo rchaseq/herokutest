1985c4850080b6838651eca9ce1af66a5b84744a
Rowan Quinn <35435338+rchaseq@users.noreply.github.com>
2022-07-11 12:06:12 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: false
-      }
+      config.resolve.fallback.fs = false;
     }
-
-    return config
-  }
+    return config;
+  },
 }
